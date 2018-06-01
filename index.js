2266d9c0543a927776edfa7e915342010226966b
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-06-02 01:29:02 +0200

    Workaround for an issue in current version of esm.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-export * from './src/index'
+// This does not work anymore with `esm` (since 3.0.35)
+// export * from './src/index'
+// instead we have to export explicitly
+export { default as compileJavascript } from './src/compileJavascript'
+export { default as JavascriptContext } from './src/JavascriptContext'
