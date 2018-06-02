cc6137a2954b4f96353a57d5f3af0054841da7f9
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-06-02 12:54:13 +0200

    Export pack/unpack

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // This does not work anymore with `esm` (since 3.0.35)
+// see standard-things/esm#444
 // export * from './src/index'
+
 // instead we have to export explicitly
 export { default as compileJavascript } from './src/compileJavascript'
 export { default as JavascriptContext } from './src/JavascriptContext'
+export { pack, unpack } from './src/types'
